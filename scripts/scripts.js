c8f927c7f9399273e79c15211def1be88b400f0f
Andrey999K <andrey1234xxxx@mail.ru>
2021-02-11 22:57:56 +0300

    add: effect menu, effect scroll

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,28 @@
-menuIcon.onclick = function myFunction() {
-    var x = document.getElementById('nav-menu');
+// menuIcon.onclick = function myFunction() {
+//     var x = document.getElementById('nav-menu');
 
-    if (x.className === "nav__menu") {
-        x.className += " responsive";
-    } else {
-        x.className = "nav__menu";
-    }
-}
+//     if (x.className === "nav__menu") {
+//         x.className += " responsive";
+//     } else {
+//         x.className = "nav__menu";
+//     }
+// }
+
+const menuButton = document.querySelector(".nav__menu");
+console.log(menuButton);
+menuButton.addEventListener("click", () => {
+    console.log("ggg");
+    menuButton.classList.toggle("responsive");
+    
+});
+
+const menu_ = document.querySelector('.nav');
+const scrollspy = new VanillaScrollspy(menu_);
+scrollspy.init();
+
+const menuLinks = document.querySelectorAll(".menu__link");
+menuLinks.forEach(item => {
+    item.addEventListener("click", () => {
+        menuButton.classList.remove("responsive");
+    });
+});
